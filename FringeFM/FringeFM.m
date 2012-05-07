abd5c1a977514bb7ae23a4e7ad4d8cd1c91b66ac
John Sheets <john@fourfringe.com>
2012-05-07 18:58:57 -0500

    Fix organization name.

--- FringeFM/FringeFM.m
+++ FringeFM/FringeFM.m
@@ -3,7 +3,7 @@
 //  FringeFM
 //
 //  Created by John Sheets on 5/7/12.
-//  Copyright (c) 2012 JAMF Software. All rights reserved.
+//  Copyright (c) 2012 John Sheets. All rights reserved.
 //
 
 #import "FringeFM.h"
