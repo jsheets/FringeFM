a19e962307be208b9162a7610d146dccdfac3fb9
John Sheets <john@fourfringe.com>
2012-05-07 17:36:53 -0500

    Initial Xcode framework project.

---
+++ FringeFM/FringeFM.h
@@ -0,0 +1,13 @@
+//
+//  FringeFM.h
+//  FringeFM
+//
+//  Created by John Sheets on 5/7/12.
+//  Copyright (c) 2012 JAMF Software. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface FringeFM : NSObject
+
+@end
---
+++ FringeFM/FringeFM.m
@@ -0,0 +1,13 @@
+//
+//  FringeFM.m
+//  FringeFM
+//
+//  Created by John Sheets on 5/7/12.
+//  Copyright (c) 2012 JAMF Software. All rights reserved.
+//
+
+#import "FringeFM.h"
+
+@implementation FringeFM
+
+@end
