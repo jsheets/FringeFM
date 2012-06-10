e92d087122dadc957a6888cb397bdd92d60cb0de
John Sheets <john@fourfringe.com>
2012-06-10 14:08:32 -0700

    Create FFMSong value object to hold data for all music services.

---
+++ FringeFM/FFMSong.m
@@ -0,0 +1,44 @@
+//
+//  FFMSong.m
+//  FringeFM
+//
+//  Created by John Sheets on 6/10/12.
+//  Copyright (c) 2012 John Sheets. All rights reserved.
+//
+// MIT License
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+// 
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+// 
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+// THE SOFTWARE.
+
+#import "FFMSong.h"
+
+@implementation FFMSong
+
+@synthesize isPlaying = _isPlaying;
+
+@synthesize artist = _artist;
+@synthesize album = _album;
+@synthesize track = _track;
+
+@synthesize artSmallUrl = _artSmallUrl;
+@synthesize artMediumUrl = _artMediumUrl;
+@synthesize artLargeUrl = _artLargeUrl;
+
+@synthesize error = _error;
+
+@end
