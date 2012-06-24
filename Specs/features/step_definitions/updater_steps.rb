3b24e3fc60316cdd846335666c6e8d4ad9e64a7b
John Sheets <john@fourfringe.com>
2012-06-23 22:22:56 -0500

    Loosen up song tests to check for any two-letter content if yml values not specified.

--- Specs/features/step_definitions/updater_steps.rb
+++ Specs/features/step_definitions/updater_steps.rb
@@ -5,9 +5,25 @@ When /^I ask for the current track$/ do
 end
 
 Then /^I get valid track info$/ do
-  @song.track.should == @track_name
-  @song.artist.should == @artist
-  @song.album.should == @album
+  # If we have a specific value we're looking for (from user_info.yml), check for that
+  # exact value. Otherwise, just make sure we have something there, so check for two or more characters.
+  if @track_name
+    @song.track.should == @track_name
+  else
+    @song.track.to_s.should match(/\w{2}/)
+  end
+
+  if @artist
+    @song.artist.should == @artist
+  else
+    @song.artist.to_s.should match(/\w{2}/)
+  end
+
+  if @album
+    @song.album.should == @album
+  else
+    @song.album.to_s.should match(/\w{2}/)
+  end
 end
 
 Then /^the (?:.+) application should be available$/ do
